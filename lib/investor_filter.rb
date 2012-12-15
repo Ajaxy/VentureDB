2b9bcb8a1ce219937c0dc9355a41be302e6e5b11
Yuri Smirnov <tycooon@yandex.ru>
2012-12-15 22:37:50 +0400

    fix investors filter

--- lib/investor_filter.rb
+++ lib/investor_filter.rb
@@ -3,7 +3,6 @@
 class InvestorFilter < Filter
   def filter(investors)
     investors = investors.search(search)  if search
-    investors = investors.in_round(round) if round
     investors = investors.in_scope(scope) if scope
     investors = investors.in_stage(stage) if stage
     investors
