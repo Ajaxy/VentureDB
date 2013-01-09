2dd0bbb9164b3ebbc383c773f5c085b420a28460
Maksim Zhylinski <uzzable@gmail.com>
2013-01-09 18:07:27 +0300

    Removed migrations thad modified data; Added dump with actual data and db:seed now uses this dump.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,14 @@
 # encoding: utf-8
-require "csv"
+# require "csv"
+#
+# require_relative "locations/seed_locations"
+# require_relative "scopes/seed_scopes"
+# require_relative "deals/seed_deals"
+# require_relative "companies/seed_companies"
+#
+# User.create!(email: "ai@grow.bi", password: "grow123").confirm!
 
-require_relative "locations/seed_locations"
-require_relative "scopes/seed_scopes"
-require_relative "deals/seed_deals"
-require_relative "companies/seed_companies"
-
-User.create!(email: "ai@grow.bi", password: "grow123").confirm!
+puts "Seeding..."
+db_config = ActiveRecord::Base.connection.instance_variable_get(:@config)
+dump_filename = File.join(Rails.root, "db", "seed.sql")
+system "psql #{db_config[:database]} < #{dump_filename}"
