da12cc235ced0638fc499258808c1ca19447ab91
Maksim Zhylinski <uzzable@gmail.com>
2013-01-09 21:34:39 +0300

    removed commened code

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,4 @@
 # encoding: utf-8
-# require "csv"
-#
-# require_relative "locations/seed_locations"
-# require_relative "scopes/seed_scopes"
-# require_relative "deals/seed_deals"
-# require_relative "companies/seed_companies"
-#
-# User.create!(email: "ai@grow.bi", password: "grow123").confirm!
 
 puts "Seeding..."
 db_config = ActiveRecord::Base.connection.instance_variable_get(:@config)
