8de72e5c9bf3f422ed43ff0843c00570e8e3030d
Maksim Zhylinski <uzzable@gmail.com>
2013-01-17 01:32:34 +0300

    Capistrano task for creating searchd config

--- config/recipes/sphinx.rb
+++ config/recipes/sphinx.rb
@@ -1,10 +1,16 @@
 before 'deploy:update_code', 'thinking_sphinx:stop'
+after 'deploy:update_code', 'sphinx:create_config'
 after 'deploy:update_code', 'thinking_sphinx:start'
 
 namespace :sphinx do
   desc "Symlink Sphinx indexes"
-  task :symlink_indexes, :roles => [:app] do
+  task :symlink_indexes, roles: [:app] do
     run "ln -nfs #{shared_path}/db/sphinx #{release_path}/db/sphinx"
+  end
+
+  desc "Create searchd config"
+  task :create_config, roles: [:app] do
+    run("cd #{deploy_to}/current && /usr/bin/env rake ts:config RAILS_ENV=production")
   end
 end
 
