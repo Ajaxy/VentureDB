6bb6a502a47e42d6b49338fbd45b053eb37b2e50
Maksim Zhylinski <uzzable@gmail.com>
2013-02-01 22:40:51 +0300

    New deploy using spearate user

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,20 +16,23 @@ require "whenever/capistrano"
 set :host, "176.58.108.251"
 server host, :web, :app, :db, primary: true
 
-set :application, "venture.staging"
+set :application, "venture"
 set :branch, "master"
 set :nginx_server_names, "venture.stage.grow.bi venturedatabase.ru " +
-                         "www.venturedatabase.ru"
+                         "www.venturedatabase.ru venture.bi"
 
-set :user, "root"
+set :user, "venture"
 set :deploy_to, -> { "/srv/#{application}" }
 set :deploy_via, :remote_cache
 set :use_sudo, false
+default_run_options[:pty] = true
 
 set :scm, "git"
 set :repository, "git@github.com:AndreyM/VentureDB.git"
 
 set :rvm_type, :system
+set :rvm_ruby_string, "ruby-1.9.3-p286@#{application}"
+
 set :postgresql_password, "111"
 
 ssh_options[:forward_agent] = true
