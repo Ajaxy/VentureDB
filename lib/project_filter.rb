b26402ccfa0c74464563789fd83385e50a58b758
RunFor <runsfor@gmail.com>
2013-03-05 00:18:22 +0700

    Fix queries for projects

--- lib/project_filter.rb
+++ lib/project_filter.rb
@@ -10,11 +10,16 @@ class ProjectFilter < Filter
       params.type = 'grants'
     end
     projects = projects.in_scopes(params.sector)      if params.sector
-    projects = projects.in_round(round)               if round
-    projects = projects.in_stage(stage)               if stage
-    projects = projects.from_amount(params.amount_start.to_i)   if params.amount_start
-    projects = projects.to_amount(params.amount_end.to_i)       if params.amount_end
-    projects = projects.for_type(type)     if type
+    projects = projects.in_rounds(params.round)       if params.round
+    projects = projects.in_stages(params.stage)       if params.stage
+
+    amount_start, amount_end = get_amount_vars params.amount_range.to_i
+    if amount_start && amount_end
+      projects = projects.in_amount_range amount_start.to_i,
+                                    amount_end.to_i
+    end
+
+    projects = projects.for_type(type)                if type
     projects = projects.sort_type(params.sort_type)
 
     projects
@@ -35,4 +40,26 @@ class ProjectFilter < Filter
       "investments" => "Инвестиции",
     }
   end
+
+  private
+
+  def get_amount_vars(range)
+    case range
+    when 1
+      return [0,50000]
+    when 2
+      return [50000,100000]
+    when 3
+      return [100000,250000]
+    when 4
+      return [250000,500000]
+    when 5
+      return [500000,1000000]
+    when 6
+      return [1000000,5000000]
+    when 7
+      return [5000000,2000000000]
+    end
+  end
+
 end
