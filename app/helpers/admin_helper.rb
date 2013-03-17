f81866095530fdd21823c90324ce1e3db98c9f1c
Maksim Zhylinski <uzzable@gmail.com>
2013-03-17 13:23:36 +0300

    Tabs to helper

---
+++ app/helpers/admin_helper.rb
@@ -0,0 +1,11 @@
+module AdminHelper
+  def tabs(*tabs)
+    content_tag :ul, class: 'nav nav-tabs' do
+      tabs.each_with_index.map do |tab, index|
+        content_tag :li, class: index.zero? ? 'active' : '' do
+          link_to tab.first, tab.second, data: { toggle: "tab" }
+        end
+      end.join.html_safe
+    end
+  end
+end
