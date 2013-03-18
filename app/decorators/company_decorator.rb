6b61d486b85fe70ef3f59bb0f617552205f5c39d
Maksim Zhylinski <uzzable@gmail.com>
2013-03-18 21:54:41 +0300

    More info on Company#show

--- app/decorators/company_decorator.rb
+++ app/decorators/company_decorator.rb
@@ -1,6 +1,22 @@
 class CompanyDecorator < ApplicationDecorator
   decorates :company
 
+  def full_name
+    source.full_name.blank? ? mdash : source.full_name
+  end
+
+  def prev_name
+    source.prev_name.blank? ? mdash : source.prev_name
+  end
+
+  def form
+    source.form.blank? ? mdash : source.form
+  end
+
+  def place
+    source.place.blank? ? mdash : source.place
+  end
+
   def type
     Company::TYPES[source.type_id]
   end
