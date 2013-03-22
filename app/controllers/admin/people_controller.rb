f6ec185d1824ef14745e7539cf858b6a37892fd3
Maksim Zhylinski <uzzable@gmail.com>
2013-03-22 22:44:01 +0300

    Fixed exception when create person

--- app/controllers/admin/people_controller.rb
+++ app/controllers/admin/people_controller.rb
@@ -19,8 +19,7 @@ class Admin::PeopleController < Admin::BaseController
   def create
     @type   = params[:type]
 
-    klass   = @type == "author" ? Author : Informer
-    @person = klass.find_or_create_draft(permitted_params.person)
+    @person = Person.find_or_create_draft(permitted_params.person)
 
     if @person.valid?
       render :success
