e5fedc6bf59d13af26e813448aa08a34cdd49b7b
Yuri Smirnov <tycooon@yandex.ru>
2013-04-18 16:56:26 +0400

    improve assets precompilation when deploying

--- config/recipes/misc.rb
+++ config/recipes/misc.rb
@@ -20,3 +20,25 @@ namespace :downloads do
   end
   after "deploy:finalize_update", "downloads:symlink"
 end
+
+# Only precompile assets if any assets files actually changed since last release
+namespace :deploy do
+  namespace :assets do
+    task :precompile, :roles => :web, :except => { :no_release => true } do
+      begin
+        from = source.next_revision(current_revision)
+        if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ lib/assets/ | wc -l").to_i > 0
+          precompile_all
+        else
+          logger.info "Skipping asset pre-compilation because there were no asset changes"
+        end
+      rescue
+        precompile_all
+      end
+    end
+
+    task :precompile_all, :roles => assets_role, :except => { :no_release => true } do
+      run "cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile"
+    end
+  end
+end
