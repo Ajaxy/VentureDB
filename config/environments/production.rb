f45cb77571e473048e2c1e47a8a53829b709ecd4
Andrey Mereskin <mereskin@gmail.com>
2013-05-14 14:59:43 +0400

    raise email errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ Venture::Application.configure do
   config.assets.initialize_on_precompile = false
 
   # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable threaded mode
   # config.threadsafe!
