12b8210e4235a08c663df5134b61f8801c7c703b
Ajaxy <alx.zinchuk@gmail.com>
2013-10-18 19:14:32 +0400

    Выгрузка в XLS

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -2,14 +2,19 @@
 
 class DealsController < CabinetController
   def index
-    search  = params[:search] ? params : params[:extended_search]
+    search = params[:search] ? params : params[:extended_search]
     @filter = decorate DealFilter.new(search), view: view_context
 
-    scope   = Deal.published.includes{[company.scopes.parent,
-                                       investors.actor, investors.locations]}
-    scope   = @filter.filter(scope)
+    scope = Deal.published.includes{[company.scopes.parent, investors.actor, investors.locations]}
+    scope = @filter.filter(scope)
+    scope = paginate(scope, 20) unless request.format.xls?
 
-    @deals  = PaginatingDecorator.decorate(paginate scope, 20)
+    @deals = PaginatingDecorator.decorate(scope)
+
+    respond_to do |format|
+      format.html
+      format.xls
+    end
   end
 
   def show
