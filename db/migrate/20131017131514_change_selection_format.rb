5f4b14180c34f9cab8fd403a864ffef585da6c07
Ajaxy <alx.zinchuk@gmail.com>
2013-10-21 18:40:27 +0400

    migration down block

--- db/migrate/20131017131514_change_selection_format.rb
+++ db/migrate/20131017131514_change_selection_format.rb
@@ -1,8 +1,15 @@
 class ChangeSelectionFormat < ActiveRecord::Migration
-  def change
+  def self.up
     change_table :selections do |t|
       t.change :format_id, :text
       t.rename :format_id, :formats
+    end
+  end
+
+  def self.down
+    change_table :selections do |t|
+      t.change :formats, :integer
+      t.rename :formats, :format_id
     end
   end
 end
